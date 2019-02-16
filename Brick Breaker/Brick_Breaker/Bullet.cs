99fad63e038a1e0c056ebdb4dbdedccbac6860c8
Idan <idan_bachar@hotmail.com>
2019-02-16 11:06:49 +0200

    Start

---
+++ Brick Breaker/Brick_Breaker/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System.Text;
+
+namespace Brick_Breaker
+{
+    public class Bullet : Shape
+    {
+        public Bullet(Rectangle rectangle, ContentManager content)
+            : base(rectangle, content) {
+
+        }
+
+        public void Move() {
+            position = new Rectangle(position.X, position.Y -= 10, position.Width, position.Height);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch) {
+            spriteBatch.Draw(texture, position, Color.Orange);
+        }
+
+    }
+}
---
+++ Brick Breaker/Brick_Breaker/Shape.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System.Text;
+
+namespace Brick_Breaker
+{
+    public abstract class Shape
+    {
+        protected Rectangle position;
+        protected Texture2D texture;
+
+        public Shape(Rectangle rectangle, ContentManager content) {
+
+            position = new Rectangle(rectangle.X, rectangle.Y, rectangle.Width, rectangle.Height);
+            texture = content.Load<Texture2D>("images/block");
+        }
+
+        public abstract void Draw(SpriteBatch spriteBatch);
+
+        public Rectangle GetPosition() {
+            return position;
+        }
+    }
+}
